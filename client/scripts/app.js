0a2622145309f7b0c0abc8a70d1841b3c94f5023
Chelsea Okey <chelsea.okey@gmail.com>
2015-05-19 09:57:46 -0500

    Angular-translate works

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,31 +1,27 @@
 var myApp = angular.module('myApp', ['pascalprecht.translate']);
+var transEN = {
+    "HEADING": "This website can be translated into German or English.",
+    "PARA": "Just click one of the buttons below to see how it works.",
+    "BUTTON_TEXT_EN": "English",
+    "BUTTON_TEXT_DE": "German"
+};
+var transDE = {
+    "HEADING": "Diese Webseite kann in deutscher oder englischer Sprache übersetzt werden.",
+    "PARA": "Klicken Sie einfach auf eine der Tasten, um zu sehen, wie es funktioniert.",
+    "BUTTON_TEXT_EN": "Englisch",
+    "BUTTON_TEXT_DE": "Deutsch"
+};
 
-myApp.config(function($translateProvider) {
-    $translateProvider.translations('en', {
-        "HEADING": "This website can be translated into German or English.",
-        "PARA": "Just click one of the buttons below to see how it works.",
-        "BUTTON_TEXT_EN": "English",
-        "BUTTON_TEXT_DE": "German"
-    })
-    .translations('de', {
-        "HEADING": "Diese Webseite kann in deutscher oder englischer Sprache übersetzt werden.",
-        "PARA": "Klicken Sie einfach auf eine der Tasten, um zu sehen, wie es funktioniert.",
-        "BUTTON_TEXT_EN": "Englisch",
-        "BUTTON_TEXT_DE": "Deutsch"
-    });
+myApp.config(['$translateProvider', function($translateProvider) {
+    $translateProvider.translations('en', transEN);
+    $translateProvider.translations('de', transDE);
     $translateProvider.preferredLanguage('en');
-});
+    $translateProvider.fallbackLanguage('en');
+    $translateProvider.useSanitizeValueStrategy(null);
+}]);
 
-//myApp.config(['$translateProvider', function ($translateProvider) {
-//    $translateProvider.useStaticFilesLoader({
-//        prefix: 'lang-',
-//        suffix: '.json'
-//    });
-//    $translateProvider.preferredLanguage('en');
-//}]);
-
-myApp.controller('langController', ["$scope", function($translate, $scope) {
-    $scope.switchLanguage = function (languageKey) {
-        $translate.uses(languageKey);
+myApp.controller('langController', ["$scope", "$translate", function($scope, $translate) {
+    $scope.changeLanguage = function (languageKey) {
+        $translate.use(languageKey);
     };
 }]);
